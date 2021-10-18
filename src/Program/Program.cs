e14c66a4f977f4be750f6772004db5594f867695
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-18 19:20:27 -0300

    fix program con fecha de cumple

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,7 @@ namespace UnitTestAndDebug
     {
         static void Main(string[] args)
         {
-            Person john = new Person("John Doe", "5.212.287-6");
+            Person john = new Person("John Doe", "5.212.287-6", "16/08/1992");
             john.IntroduceYourself();
         }
     }
